07319bb89ddc8994804bb7bffe6b5c3d85148318
Jedsadakorn Yonchorhor <j.yonchor@gmail.com>
2021-04-12 23:30:46 +0900

    tello_crop train config

--- _train_in_docker.sh
+++ _train_in_docker.sh
@@ -1,10 +1,10 @@
 # /usr/bin/python -m pip install --upgrade pip
-# sudo pip install --upgrade wandb
+sudo pip install --upgrade wandb
 wandb login c7a16e7705748cd60106adbb57f03a601ef3f828
 
 # export WANDB_NAME="Let's graduate"
 export WANDB_ENTITY="surfii3z"
-export WANDB_PROJECT="thesis"
+export WANDB_PROJECT="thesis_tello_crop"
 export WANDB_API_KEY="c7a16e7705748cd60106adbb57f03a601ef3f828"
 # export WANDB_RESUME="allow"
 # export WANDB_RUN_ID="c038edb911820e40b927968ca8e89ae2ad6cb6da"
